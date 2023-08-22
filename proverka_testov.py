525af50a846ebd6dc5286c239daa20a1c3aebf8d
VaT-10 <138997593+VaT-10@users.noreply.github.com>
2023-08-22 15:44:58 +0300

    proverka_testov_update.py
    ОБНОВЛЕНИЕ!!!
    + Исправление ошибок
    + Поддерживание регулярных выражений
    + Вывод количества правильных и не правильных ответов
    + И другое!

--- proverka_testov.py
+++ proverka_testov.py
@@ -4,17 +4,18 @@
 from zipfile import ZipFile
 import sys
 
-my_code = """..."""
-filename = '...'
+my_code = r'''...'''
+filename = '...' + '.zip'
 
 orig_std = sys.stdout
-
+count_true = 0
+count_false = 0
 
 
 def read_my_clue():
     sys.stdout = open('res.txt', 'w')
     exec(new_my_code, globals())
-    if 'input' not in my_code:
+    if flag:
         exec(test)
     sys.stdout = orig_std
     with open('res.txt') as res_code:
@@ -23,7 +24,9 @@ def read_my_clue():
 
 try:
     with ZipFile(filename) as file:
-        tests = filter(str.isdigit, file.namelist())
+        flag = input('Есть ли код в тестах? (да/нет)\n') == 'да'
+        namelist = file.namelist()
+        tests = filter(str.isdigit, namelist)
 
         for i in tests:
             new_my_code = my_code
@@ -32,30 +35,32 @@ try:
 
             with file.open(i) as f:
                 test = f.read().decode('utf-8')
-                if 'input' in my_code:
+                if not flag:
                     for j in test.split('\n'):
                         new_my_code = new_my_code.replace('input()', repr(j.strip('\r')), 1)
             try:
                 my_clue = read_my_clue()
             except Exception as e:
+                count_false += 1
                 sys.stdout = orig_std
                 error = str(type(e)).strip('<class \'')[:-2]
-                print(f'ТЕСТ {i}: FAIL')
+                print(f'ТЕСТ {i}/{len(namelist) // 2}: FAIL')
                 print('\nТест:')
                 print(test)
                 print(f'\nПри выполнении кода произошла ошибка: {error}: {e}')
                 print('Правильный ответ:')
                 print(right_clue)
-
-                continue_or_no = input('Продолжать проверку? (да/нет)\n')
-                if continue_or_no.lower() != 'да':
-                    break
-                else:
-                    continue
+                if namelist[-2] != i:
+                    continue_or_no = input('Продолжать проверку? (да/нет)\n')
+                    if continue_or_no.lower() != 'да':
+                        break
+                continue
             if my_clue.strip('\n') == right_clue:
-                print(f'ТЕСТ {i}: OK✔')
+                count_true += 1
+                print(f'ТЕСТ {i}/{len(namelist) // 2}: OK✔')
             else:
-                print(f'ТЕСТ {i}: FAIL')
+                count_false += 1
+                print(f'ТЕСТ {i}/{len(namelist) // 2}: FAIL')
                 print('\nТест:')
                 print(test)
                 print('\nВаш ответ:')
@@ -65,6 +70,7 @@ try:
                 continue_or_no = input('Продолжать проверку? (да/нет)\n')
                 if continue_or_no.lower() != 'да':
                     break
+    print(f'\nПроверено тестов: {i}/{len(namelist) // 2}\nКоличество правильных ответов: {count_true}/{i}\nКоличество неправильных ответов: {count_false}/{i}')
 except FileNotFoundError:
     print('Архив не найден.')
 except:
